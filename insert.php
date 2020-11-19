95901a86f5c806cd08b4c8447bc5df71d09e78e4
63070077 <73679335+63070077@users.noreply.github.com>
2020-11-19 20:47:05 +0700

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 $conn = mysqli_init();
-mysqli_real_connect($conn, 'phufarpsw.mysql.database.azure.com', 'phufarpsw@phufarpsw', 'EMLbmk28', 'itflab', 3306);
+mysqli_real_connect($conn, 'mindtanavich.mysql.database.azure.com', 'mindeiei@mindtanavich', 'Kjom1234', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
